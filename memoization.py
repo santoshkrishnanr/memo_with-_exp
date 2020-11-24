1ce228492d8d1be27a4beeaebc2f46997aaff993
santoshkrishnanr <santoshkrishnanr@github.com>
2020-11-25 00:16:49 +0100

    initial python working with timeout .
    values are stored with timeout (expiry time) instead to executed timeout so each function value can have different expiry date.
    
    different functions can also be added with different arguments and timeout .

--- memoization.py
+++ memoization.py
@@ -1,39 +1,40 @@
-# a basic program to show memoizaton using python3.7
+# a basic program to show memoizaton using python3.7 with time out
+
 
 import time
+
 memory = {}
 
-def timenow():
+
+# to get the time in ms
+def time_now():
     return int(round(time.time() * 1000))
 
-# some function taking arguments and returning data..
-def addToTime(year,month,day):
-    totalday = ((year*365) + (month*31) + (day))*86400000
-    print('from function')
-    return totalday
+# caching
+def memoize(fun, year, month, day, timeout):
+    # unique address to save the value i.e resolver
+    key = int(str(year) + str(month) + str(day))
 
-
-def memoize(year,month,day):
-    key = year+month+day
-
-    if (key in memory):
-        if ((timenow() - memory[key]['time']) <= 5000):
+    if key in memory:
+        if time_now() <= memory[key]['time']:
             print('memory')
             return memory[key]['value']
 
-    else:
-        result = addToTime(year, month, day)
-        memory.update({key:{'time': timenow(), 'value':result}})
-        return result
+    result = fun(year, month, day)
+    memory.update({key: {'time': (time_now() + timeout), 'value': result}})
+    return result
 
 
+# some function taking arguments and returning data..
+def add_to_time(year, month, day):
+    totaldays = (((year * 365) + (month * 31) + day) * 86400000) + time_now()
+    print('from function')
+    return totaldays
 
 
+if __name__ == "__main__":
+    print(memoize(add_to_time, 0, 1, 00, 5000))
+    time.sleep(5)
+    print(memoize(add_to_time, 0, 1, 00, 5000))
+    time.sleep(0)
 
-
-
-
-print(memoize(1, 1, 30))
-time.sleep(5)
-
-print(memoize(1, 1, 30))
