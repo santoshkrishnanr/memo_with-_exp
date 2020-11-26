c1d75e8cbc565a8120cea7bffd7571ac0ed37bb5
santoshkrishnanr <santoshkrishnanr@github.com>
2020-11-26 21:04:33 +0100

    memoization main file with function.

--- memoization.py
+++ memoization.py
@@ -1,8 +1,8 @@
-# a basic program to show memoizaton using python3.7 with time out
-
+# a basic program to show memoization using python3.7 with time out
 
 import time
 
+# to store the given output with its time out value
 memory = {}
 
 
@@ -14,12 +14,11 @@ def time_now():
 # caching
 def memoize(fun, year, month, day, timeout):
     # unique address to save the value i.e resolver
-    key = resolver(year,month,day)
+    key = resolver(year, month, day)
 
     if key in memory:
         if time_now() <= memory[key]['time']:
-            #print('memory')
-            #time.sleep(3)
+            # print('memory')
             return memory[key]['value']
 
     result = fun(year, month, day)
@@ -27,24 +26,23 @@ def memoize(fun, year, month, day, timeout):
     return result
 
 
+# converting the arguments to a hexa value as address for the output function
 def resolver(year, month, day):
     return (hex(year) + hex(month) + hex(day))
 
 
-# some function taking arguments and returning data..
+# some function taking arguments and returning data..(to take total millisecond in sum of year,month,day)
 def add_to_time(year, month, day):
     totaldays = (((year * 365) + (month * 31) + day) * 86400000) + time_now()
-    #print('from function')
-    #time.sleep(7)
+    time.sleep(0.001)  # time to run this function
     return totaldays
 
 
 if __name__ == "__main__":
-
+    """"" test case to call the function with some argument and calling again to get from memory.
+     and checking again after the time out """""
 
     print(memoize(add_to_time, 0, 1, 00, 5000))
     print(memoize(add_to_time, 0, 1, 00, 5000))
     time.sleep(5)
     print(memoize(add_to_time, 0, 1, 00, 5000))
-
-
