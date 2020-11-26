7f3d8c6a61e41c7bb2bd240d4270bcde5aee125c
santoshkrishnanr <santoshkrishnanr@github.com>
2020-11-26 15:46:19 +0100

    TODO- sending function and timeout

--- memoizaton_.js
+++ memoizaton_.js
@@ -1,21 +1,24 @@
+
+// memoize function to take a function as argument.
 const memoize = (fn) => {
     const memory = new Map()
 
     return function(a,b,c){
         //unique address to store value (resolver)
-        x= dec2hexString(a)+dec2hexString(b)+dec2hexString(c);
+        x = a.toString(16)+b.toString(16)+c.toString(16);
 
         // if there is some value in this unique address in memory 
         if(memory.has(x)){
             tem=memory.get(x)
-            // if that value stored from last 5000 ms.
+
+            // to check if that value stored from last 5000 ms.
             if (Date.now()-(tem.Time) <= 5000){
                 console.log('from memory ');
                 return memory.get(x);
             }
         }
 
- // if not in memory run the function with given agrgument 
+ // if not in memory run the function with given parameter
         const result = fn(a,b,c)
         //saving the output of function with timestamp TIME: and value:
         memory.set(x,{Time: Date.now(),value:result})
@@ -23,39 +26,34 @@ const memoize = (fn) => {
     };
 };
     
-// to have delay 
-function sleep(miliseconds) {
+// to have delay in milliseconds
+function sleep(milliseconds) {
     var currentTime = new Date().getTime();
  
-    while (currentTime + miliseconds >= new Date().getTime()) {
+    while (currentTime + milliseconds >= new Date().getTime()) {
     }
  }
 
-//function doing something and returning value in this case current date and time  
 
+//function doing something and returning value in this case current date and time
 const addToTime = (year, month,day ) => {
-
     console.log('i ran this function with arg  '+year,month, day  );
-    // return data is stored as value: in this case  
+    // return data is stored as value: in this case (datenow+ datetime)
     return Date.now() + Date()
 }
 
+
+
 //const fastaddToTime = memoize(addToTime,(year, month, day) => year + month + day, 5000);
+// in this case function and timeout is not sent as argument.
 const fastaddToTime = memoize(addToTime);
 
 
 
-// example to show the usage of memory
-console.log(fastaddToTime(1,11,26));
-console.log(fastaddToTime(1,11,6));
-// example to show calling the same function after 5000 ms to run the function again
-sleep(5000);
-console.log(fastaddToTime(1,11,26));
-
-
 // calling the function multiple times to show the usage of memory
+// after 5000 ms the result is updated for same argument.
 console.log(fastaddToTime(1,10,26));
 console.log(fastaddToTime(1,10,26));
-sleep(4000);
+sleep(5000);
 console.log(fastaddToTime(1,10,26));
 
